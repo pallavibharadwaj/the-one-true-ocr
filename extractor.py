0c55a12bee0c7a8cc4a1a171cae633b687704d87
heartz <ranadip.rulz@gmail.com>
2016-04-20 14:00:55 +0530

    Fix all the pep8 issues in the code

--- extractor.py
+++ extractor.py
@@ -1,80 +1,94 @@
-def find_eular(each_segment,segments):
-	'''Finding the Euler Number of a segment'''
-	flag=0
-	x1,y1,w1,h1=each_segment
-	x2=x1+w1
-	y2=y1+h1
-	for segment in segments :
-		if ((segment==each_segment).all()):
-			continue
-		x,y,w,h=segment
-		if x>x1 and x<x2 and y>y1 and y<y2 and x+w>x1 and x+w<x2 and y+h>y1 and y+h<y2  :
-			flag=flag+1
-	euler_number=1-flag
-	return euler_number
+def find_eular(each_segment, segments):
+    '''Finding the Euler Number of a segment'''
+    flag = 0
+    x1, y1, w1, h1 = each_segment
+    x2 = x1+w1
+    y2 = y1+h1
+    for segment in segments:
+        if ((segment == each_segment).all()):
+            continue
+        x, y, w, h = segment
+        if (
+            x > x1 and
+            x < x2 and
+            y > y1 and
+            y < y2 and
+            x+w > x1 and
+            x+w < x2 and
+            y+h > y1 and
+            y+h < y2
+        ):
+            flag = flag+1
+    euler_number = 1-flag
+    return euler_number
 
-def find_total_on_pixels(image,segment):
-	'''Finding the total number of on_pixels in a segment'''
-	on_pixel=0
-	x,y,w,h=segment
-	for i in range(y, y+h) :
-		for j in  range(x, x+w) :
-			pixel_intensity= image[i,j]	#white(255) or black(0)
-			#if the pixel is black, find distance from the axis center
-			if pixel_intensity==0 :
-				on_pixel+=1
-	return on_pixel
 
-def extract_coordinate_based_features(image,on_pixel,segment):
-	'''Finding all the features based of the x,y of on_pixels in a segment'''
-	x,y,w,h=segment
-	horizontal_sum=0.0
-	vertical_sum=0.0
-	horizontal_square_sum=0.0
-	vertical_square_sum=0.0
-	xy_correlation_sum=0.0
-	xxy_sum=0.0
-	yyx_sum=0.0
-	central_y_axis=x+w/2 # the line is where x = 0
-	central_x_axis=y+h/2 # the line is where y = 0
-	for i in range(y, y+h) :
-		for j in  range(x, x+w) :
-			pixel_intensity= image[i,j]	#white(255) or black(0)
-			#if the pixel is black, find distance from the axis center
-			if pixel_intensity==0 :
-				horizontal_dist=j- central_y_axis	#negative for left and positive for right of center
-				horizontal_sum+=horizontal_dist	#negative if left heavy
-				vertical_dist=central_x_axis- i	#positive for up and negative for down of center
-				vertical_sum+=vertical_dist		#positive if up heavy
-				horizontal_square_sum+=((horizontal_dist)*(horizontal_dist))
-				vertical_square_sum+=((vertical_dist)*(vertical_dist))
-				xy_correlation=horizontal_dist*vertical_dist
-				xy_correlation_sum+=xy_correlation
-				xxy_value=(horizontal_dist*horizontal_dist)*vertical_dist
-				yyx_value=(vertical_dist*vertical_dist)*horizontal_dist
-				xxy_sum+=xxy_value
-				yyx_sum+=yyx_value
-	horizontal_mean=horizontal_sum/(w*on_pixel)           #divide by width of the segment
-	vertical_mean=vertical_sum/(h*on_pixel)               #divide by height of the segment
-	horizontal_variance=horizontal_square_sum/on_pixel
-	vertical_variance=vertical_square_sum/on_pixel
-	xy_correlation_mean=xy_correlation_sum/on_pixel
-	xxy_mean=xxy_sum/on_pixel
-	yyx_mean=yyx_sum/on_pixel
-	return ([horizontal_mean,x,y,w,h,vertical_mean,horizontal_variance,vertical_variance,xy_correlation_mean,xxy_mean,yyx_mean])
+def find_total_on_pixels(image, segment):
+    '''Finding the total number of on_pixels in a segment'''
+    on_pixel = 0
+    x, y, w, h = segment
+    for i in range(y, y+h):
+        for j in range(x, x+w):
+            pixel_intensity = image[i, j]  # white(255) or black(0)
+            # if the pixel is black, find distance from the axis center
+            if pixel_intensity == 0:
+                on_pixel += 1
+    return on_pixel
+
+
+def extract_coordinate_based_features(image, on_pixel, segment):
+    '''Finding all the features based of the x,y of on_pixels in a segment'''
+    x, y, w, h = segment
+    horizontal_sum = 0.0
+    vertical_sum = 0.0
+    horizontal_square_sum = 0.0
+    vertical_square_sum = 0.0
+    xy_correlation_sum = 0.0
+    xxy_sum = 0.0
+    yyx_sum = 0.0
+    central_y_axis = x+w/2  # the line is where x = 0
+    central_x_axis = y+h/2  # the line is where y = 0
+    for i in range(y, y+h):
+        for j in range(x, x+w):
+            pixel_intensity = image[i, j]    # white(255) or black(0)
+            # if the pixel is black, find distance from the axis center
+            if pixel_intensity == 0:
+                horizontal_dist = j - central_y_axis
+                horizontal_sum += horizontal_dist
+                vertical_dist = central_x_axis - i
+                vertical_sum += vertical_dist
+                horizontal_square_sum += ((horizontal_dist)*(horizontal_dist))
+                vertical_square_sum += ((vertical_dist)*(vertical_dist))
+                xy_correlation = horizontal_dist*vertical_dist
+                xy_correlation_sum += xy_correlation
+                xxy_value = (horizontal_dist*horizontal_dist)*vertical_dist
+                yyx_value = (vertical_dist*vertical_dist)*horizontal_dist
+                xxy_sum += xxy_value
+                yyx_sum += yyx_value
+    horizontal_mean = horizontal_sum/(w*on_pixel)
+    vertical_mean = vertical_sum/(h*on_pixel)
+    horizontal_variance = horizontal_square_sum/on_pixel
+    vertical_variance = vertical_square_sum/on_pixel
+    xy_correlation_mean = xy_correlation_sum/on_pixel
+    xxy_mean = xxy_sum/on_pixel
+    yyx_mean = yyx_sum/on_pixel
+    return ([
+        horizontal_mean, x, y, w, h, vertical_mean, horizontal_variance,
+        vertical_variance, xy_correlation_mean, xxy_mean, yyx_mean
+    ])
 
 
 '''def horizontal_edge_features(segment):
-	on_pixel=0
-	x,y,w,h=segment
-	for i in range(y, y+h) :
-		for j in  range(x, x+w) :
-			pixel_intensity= image[i,j]	#white(255) or black(0)
-			#if the pixel is black, find distance from the axis center
-			if pixel_intensity==0 :
-				if image[i-1][j]==255 :
-					horizontal_edge_count+=1
-					horizontal_edge_dist=j-central_y_axis
-					horizontal_edge_dist_sum+=horizontal_edge_dist
-	horizontal_edge_dist_mean = horizontal_edge_dist_sum/horizontal_edge_count
-	return horizontal_edge_dist_mean'''
+    on_pixel=0
+    x,y,w,h=segment
+    for i in range(y, y+h) :
+        for j in  range(x, x+w) :
+            pixel_intensity= image[i,j]    #white(255) or black(0)
+            #if the pixel is black, find distance from the axis center
+            if pixel_intensity==0 :
+                if image[i-1][j]==255 :
+                    horizontal_edge_count+=1
+                    horizontal_edge_dist=j-central_y_axis
+                    horizontal_edge_dist_sum+=horizontal_edge_dist
+    horizontal_edge_dist_mean = horizontal_edge_dist_sum/horizontal_edge_count
+    return horizontal_edge_dist_mean'''
--- main.py
+++ main.py
@@ -1,63 +1,86 @@
 import cv2
 import numpy
-from extractor import find_eular,find_total_on_pixels,extract_coordinate_based_features
-SEGMENTS_DIRECTION= 0 # vertical axis in numpy
+from extractor import find_eular, find_total_on_pixels
+from extractor import extract_coordinate_based_features
+SEGMENTS_DIRECTION = 0  # vertical axis in numpy
 
-def segments_to_numpy( segments ):
+
+def segments_to_numpy(segments):
     '''given a list of 4-element tuples, transforms it into a numpy array'''
-    segments= numpy.array( segments, dtype=numpy.uint16 , ndmin=2)   #each segment in a row
-    segments= segments if SEGMENTS_DIRECTION==0 else numpy.transpose(segments)
+    segments = numpy.array(segments, dtype=numpy.uint16, ndmin=2)
+    # each segment in a row
+    if SEGMENTS_DIRECTION != 0:
+        numpy.transpose(segments)
     return segments
 
-def draw_segments( image , segments,color=(255,0,0), line_width=1):
+
+def draw_segments(image, segments, color=(255, 0, 0), line_width=1):
         '''draws segments on image'''
         for segment in segments:
-            x,y,w,h= segment
-            cv2.rectangle(image,(x,y),(x+w,y+h),color,line_width)
+            x, y, w, h = segment
+            cv2.rectangle(image, (x, y), (x+w, y+h), color, line_width)
 
 image = cv2.imread('alpha.png')
-cv2.imshow('Display',image)
-copy = image.copy() # Keeping a non-greyscale copy of the image for later use
+cv2.imshow('Display', image)
+copy = image.copy()  # Keeping a non-greyscale copy of the image for later use
 cv2.waitKey(0)
-image = cv2.GaussianBlur(image,(5,5),0) # Blurring the image
-cv2.imshow('Display',image)
+image = cv2.GaussianBlur(image, (5, 5), 0)  # Blurring the image
+cv2.imshow('Display', image)
 print("After Guassian blurring")
 cv2.waitKey(0)
-image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) # Grey scaling the image
-cv2.imshow('Display',image) # Displaying the image
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Grey scaling the image
+cv2.imshow('Display', image)  # Displaying the image
 print("After Greyscaling")
 cv2.waitKey(0)
-image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 10)
-'''Thresholding the image (Converts greyscale to binary), using adaptive threshold for best results '''
-im2, contours, hierarchy = cv2.findContours(image,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+image = cv2.adaptiveThreshold(
+    image,
+    255,
+    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    cv2.THRESH_BINARY,
+    11, 10)
+
+''' Thresholding the image (Converts greyscale to binary),
+    using adaptive threshold for best results '''
+
+im2, contours, hierarchy = cv2.findContours(
+    image,
+    cv2.RETR_LIST,
+    cv2.CHAIN_APPROX_SIMPLE)
+
 ''' Finding the contours in the image'''
+
 image.fill(255)
-cv2.drawContours(image, contours, -1, (0,0,255))
+cv2.drawContours(image, contours, -1, (0, 0, 255))
 ''' Drawing the contours on the image before displaying'''
-cv2.imshow('Display',image)
+cv2.imshow('Display', image)
 print("After detecting Contours")
 cv2.waitKey(0)
-segments= segments_to_numpy( [cv2.boundingRect(c) for c in contours] )
+segments = segments_to_numpy([cv2.boundingRect(c) for c in contours])
 ''' Convert each segment to x,y,w,h (4-element tuple for numpy)'''
-draw_segments(copy,segments)
+draw_segments(copy, segments)
 '''Draw the segments on the copy image (cant add color to greyscaled image)'''
-cv2.imshow('Display',copy)
+cv2.imshow('Display', copy)
 print("After Segmentation")
 cv2.waitKey(0)
-#Finding the outer bounding box to find segments within a segment 
-xmin,ymin,wmin,hmin= numpy.amin(segments, axis=0) 
-xmax,ymax,wmax,hmax= numpy.amax(segments, axis=0) 
-print "number of segments:{}".format(len(segments)) #number of segments for reference and confirmation
+# Finding the outer bounding box to find segments within a segment
+xmin, ymin, wmin, hmin = numpy.amin(segments, axis=0)
+xmax, ymax, wmax, hmax = numpy.amax(segments, axis=0)
+print "number of segments:{}".format(len(segments))
+# number of segments for reference and confirmation
 print "Feature Data: Euler Number, Horizontal Mean, Vertical Mean"
-feature_list =[]
-for each_segment in segments :
-	if (each_segment==[xmin,ymin,wmax,hmax]).all(): # Skipping the large segment
-		continue
-	euler_number=find_eular(each_segment,segments)
-	on_pixel=find_total_on_pixels(image,each_segment)
-	coordinate_features = extract_coordinate_based_features(image,on_pixel,each_segment);
-	# Converting all the feature data into a tuple
-	feature_list.append([euler_number,on_pixel]+coordinate_features)
+feature_list = []
+for each_segment in segments:
+    if (each_segment == [xmin, ymin, wmax, hmax]).all():
+        # Skipping the large segment
+        continue
+    euler_number = find_eular(each_segment, segments)
+    on_pixel = find_total_on_pixels(image, each_segment)
+    coordinate_features = extract_coordinate_based_features(
+        image,
+        on_pixel,
+        each_segment)
+    # Converting all the feature data into a tuple
+    feature_list.append([euler_number, on_pixel]+coordinate_features)
 print feature_list
 cv2.waitKey(0)
 cv2.destroyAllWindows()
