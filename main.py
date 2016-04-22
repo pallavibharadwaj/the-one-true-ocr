7540b132dd48dee15662869c22551c50582a2ce2
heartz <ranadip.rulz@gmail.com>
2016-04-22 19:52:35 +0530

    Add function to get character from user for each segment

--- main.py
+++ main.py
@@ -15,10 +15,29 @@ def segments_to_numpy(segments):
 
 
 def draw_segments(image, segments, color=(255, 0, 0), line_width=1):
-        '''draws segments on image'''
-        for segment in segments:
-            x, y, w, h = segment
-            cv2.rectangle(image, (x, y), (x+w, y+h), color, line_width)
+    '''draws segments on image'''
+    for segment in segments:
+        x, y, w, h = segment
+        cv2.rectangle(image, (x, y), (x+w, y+h), color, line_width)
+
+
+def draw_individual_segment(image, segment, color=(0, 0, 255), line_width=1):
+    '''draws a rectangle around the current segment'''
+    x, y, w, h = segment
+    copy_image = image.copy()
+    cv2.rectangle(copy_image, (x, y), (x+w, y+h), color, line_width)
+    cv2.imshow('Display', copy_image)
+
+
+def get_char(image, segment, segment_feature):
+    '''getting the character from user for the segment'''
+    key_list = []
+    draw_individual_segment(image, segment)
+    key = cv2.waitKey(0)
+    key %= 256
+    key_list.append(key)
+    return unichr(key)
+
 
 image = cv2.imread('alpha.png')
 cv2.imshow('Display', image)
@@ -55,8 +74,10 @@ cv2.drawContours(image, contours, -1, (0, 0, 255))
 cv2.imshow('Display', image)
 print("After detecting Contours")
 cv2.waitKey(0)
+contours.reverse()
 segments = segments_to_numpy([cv2.boundingRect(c) for c in contours])
 ''' Convert each segment to x,y,w,h (4-element tuple for numpy)'''
+copy_for_grounding = copy.copy()
 draw_segments(copy, segments)
 '''Draw the segments on the copy image (cant add color to greyscaled image)'''
 cv2.imshow('Display', copy)
@@ -65,9 +86,6 @@ cv2.waitKey(0)
 # Finding the outer bounding box to find segments within a segment
 xmin, ymin, wmin, hmin = numpy.amin(segments, axis=0)
 xmax, ymax, wmax, hmax = numpy.amax(segments, axis=0)
-print "number of segments:{}".format(len(segments))
-# number of segments for reference and confirmation
-print "Feature Data: Euler Number, Horizontal Mean, Vertical Mean"
 feature_list = []
 for each_segment in segments:
     if (each_segment == [xmin, ymin, wmax, hmax]).all():
@@ -80,7 +98,12 @@ for each_segment in segments:
         on_pixel,
         each_segment)
     # Converting all the feature data into a tuple
-    feature_list.append([euler_number, on_pixel]+coordinate_features)
-print feature_list
+    segment_features = [euler_number, on_pixel]+coordinate_features
+    char_data = get_char(copy_for_grounding, each_segment, segment_features)
+    final_data = [char_data] + segment_features
+    print(final_data)
+    feature_list.append(final_data)
+    # draw_individual_segment(copy_for_grounding, each_segment)
+# print feature_list
 cv2.waitKey(0)
 cv2.destroyAllWindows()
