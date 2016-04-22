719cc28910fc957a2dc277f3fba74b84c70e9a04
heartz <ranadip.rulz@gmail.com>
2016-04-23 02:28:59 +0530

    Knn seems to be implemented, but nah

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from segmentation import segments_to_numpy, draw_segments
 
 f=open("data.txt",'w')
 
-image = cv2.imread('alpha.png')
+image = cv2.imread('train1.png')
+image2 = cv2.imread('test1.png')
 cv2.imshow('Display', image)
 copy = image.copy()  # Keeping a non-greyscale copy of the image for later use
 cv2.waitKey(0)
 image = cv2.GaussianBlur(image, (5, 5), 0)  # Blurring the image
+image2 = cv2.GaussianBlur(image2, (5,5), 0)
 cv2.imshow('Display', image)
 print("After Guassian blurring")
 cv2.waitKey(0)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Grey scaling the image
+image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Display', image)  # Displaying the image
 print("After Greyscaling")
 cv2.waitKey(0)
@@ -24,25 +27,37 @@ image = cv2.adaptiveThreshold(
     cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
     cv2.THRESH_BINARY,
     11, 10)
-
+image2 = cv2.adaptiveThreshold(
+    image2,
+    255,
+    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    cv2.THRESH_BINARY,
+    11, 10)
 ''' Thresholding the image (Converts greyscale to binary),
     using adaptive threshold for best results '''
-
+cv2.imshow('Display', image)
 im2, contours, hierarchy = cv2.findContours(
     image,
     cv2.RETR_LIST,
     cv2.CHAIN_APPROX_SIMPLE)
-
+im22, contours2, hierarchy2 = cv2.findContours(
+    image2,
+    cv2.RETR_LIST,
+    cv2.CHAIN_APPROX_SIMPLE)
 ''' Finding the contours in the image'''
 
 image.fill(255)
+image2.fill(255)
 cv2.drawContours(image, contours, -1, (0, 0, 255))
+cv2.drawContours(image2, contours2, -1, (0, 0, 255))
 ''' Drawing the contours on the image before displaying'''
 cv2.imshow('Display', image)
 print("After detecting Contours")
 cv2.waitKey(0)
 contours.reverse()
+contours2.reverse()
 segments = segments_to_numpy([cv2.boundingRect(c) for c in contours])
+segments2 = segments_to_numpy([cv2.boundingRect(c) for c in contours2])
 ''' Convert each segment to x,y,w,h (4-element tuple for numpy)'''
 copy_for_grounding = copy.copy()
 draw_segments(copy, segments)
@@ -54,7 +69,10 @@ cv2.waitKey(0)
 xmin, ymin, wmin, hmin = numpy.amin(segments, axis=0)
 xmax, ymax, wmax, hmax = numpy.amax(segments, axis=0)
 feature_list = []
-
+classes_list = []
+feature_list2 = []
+classes_list2 = []
+knn = cv2.ml.KNearest_create()
 for each_segment in segments:
     if (each_segment == [xmin, ymin, wmax, hmax]).all():
         # Skipping the large segment
@@ -65,14 +83,6 @@ inner_segments= inner_segment()
 
 flag=0
 for each_segment in segments:
-    #if (each_segment == [xmin, ymin, wmax, hmax]).all():
-    #    # Skipping the large segment
-    #    continue
-    #euler_number = find_eular(each_segment, segments)
-    #add code to remove inner segments
-    #inner_segments= inner_segment()
-    #print inner_segments
-    #print type(inner_segments)
     bad_flag=0
     x,y,w,h=each_segment
     for inner in inner_segments:
@@ -95,10 +105,75 @@ for each_segment in segments:
     # Converting all the feature data into a tuple
     segment_features = [euler_number, on_pixel]+coordinate_features
     char_data = get_char(copy_for_grounding, each_segment, segment_features)
-    final_data = [char_data] + segment_features
-    f.write(' '.join(map(str, final_data))+"\n")
-    print(final_data)
-print flag
+    final_data = segment_features
+    feature_list.append(final_data)
+    classes_list.append(ord(char_data))
+    # f.write(' '.join(map(str, final_data))+"\n")
+
+
+
+for each_segment in segments2:
+    if (each_segment == [xmin, ymin, wmax, hmax]).all():
+        # Skipping the large segment
+        continue
+    euler_number = find_eular(each_segment, segments2)
+#add code to remove inner segments
+inner_segments= inner_segment()
+
+flag=0
+
+for each_segment in segments2:
+    bad_flag=0
+    x,y,w,h=each_segment
+    for inner in inner_segments:
+        x1,y1,w1,h1=inner
+        if ([x,y,w,h]==[x1,y1,w1,h1]):
+            flag=flag+1
+            bad_flag=1
+            continue
+    if(bad_flag==1):
+        continue
+    if (each_segment == [xmin, ymin, wmax, hmax]).all():
+        # Skipping the large segment
+        continue
+    euler_number = find_eular(each_segment, segments2)
+    on_pixel = find_total_on_pixels(image2, each_segment)
+    cv2.waitKey(0)
+    coordinate_features = extract_coordinate_based_features(
+        image2,
+        on_pixel,
+        each_segment)
+    # Converting all the feature data into a tuple
+    segment_features = [euler_number, on_pixel]+coordinate_features
+    #char_data = get_char(copy_for_grounding, each_segment, segment_features)
+    final_data = segment_features
+    feature_list2.append(final_data)
+    classes_list2.append(ord(char_data))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+features= numpy.asarray( feature_list, dtype=numpy.float32 )
+classes= numpy.asarray( classes_list, dtype=numpy.float32 )
+features2 =  numpy.asarray( feature_list2, dtype=numpy.float32 )
+print('haha',features)
+print('help',features2)
+knn.train(features,cv2.ml.ROW_SAMPLE, classes)
+retval, result_classes, neigh_resp, dists= knn.findNearest(features2, k= 1)
+temp_classes = result_classes.tolist()
+flattened = [chr(int(val)) for sublist in temp_classes for val in sublist]
+print flattened
+
     # draw_individual_segment(copy_for_grounding, each_segment)
 # print feature_list
 f.close()
