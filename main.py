508f77d3a2515f4faf504d148153b7e4d2c46b81
bharadwajpallavi <pallavi.bharadwaj1594@gmail.com>
2016-04-27 01:48:45 +0530

    wrapping around the imread-done

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ import os
 from extractor import get_feature_list, get_class_list
 from preprocessor import preprocess, preprocess_with_display
 from files import load_data_from_file, generate_ground_data
+image_1="data/alpha.png"
+image_2="data/alpha2.png"
 
-image = cv2.imread('data/alpha.png')
-image2 = cv2.imread('data/alpha2.png')
-img,extension=os.path.splitext("data/alpha.png") #splits the image path and the extension
-#print img
+def read_image(image_1,image_2) :
+	image = cv2.imread("%s" % image_1)
+	image2 = cv2.imread("%s" %image_2)
+	return image,image2
+
+image,image2 = read_image(image_1,image_2)
+img,extension=os.path.splitext("%s" %image_1) #splits the image path and the extension
+#print img #data/alpha
 in_file = os.path.basename(img) #extracts the filename from an extension
-#print in_file
+#print in_file #alpha
 file = open('%s.box' % in_file,'a')
 copy=image.copy()
 image, segments = preprocess_with_display(image)
