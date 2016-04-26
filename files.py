807dec59efeac54fdcb65d3d9ccdaae31806e39f
heartz <ranadip.rulz@gmail.com>
2016-04-26 15:35:17 +0530

    Add .gitignore, Move image and data files to /data

--- files.py
+++ files.py
@@ -3,15 +3,17 @@ from extractor import get_feature_list, get_class_list
 def generate_ground_data(image,copy, segments):
     feature_list= get_feature_list(image, segments)
     classes_list= get_class_list(copy, segments)
-    with open("test.txt",'wb') as test :
+    with open("data/alpha.txt",'wb') as test :
         for char,feature in zip(classes_list,feature_list) :
-                test.write("%s %s\n" %(chr(char), ' '.join(map(str,feature))))
+            if(char==32):
+                char=46
+            test.write("%s %s\n" %(chr(char), ' '.join(map(str,feature))))
 
 def load_data_from_file():
-    with open("test.txt") as char_file:
+    with open("data/alpha.txt") as char_file:
     	ncols = len(char_file.readline().split(' '))
-    classes = numpy.loadtxt("test.txt", dtype = str, usecols = [0])
-    features = numpy.loadtxt("test.txt", dtype = float, usecols = range(1,ncols))
+    classes = numpy.loadtxt("data/alpha.txt", dtype = str, usecols = [0])
+    features = numpy.loadtxt("data/alpha.txt", dtype = float, usecols = range(1,ncols))
     classes = [ord(x) for x in classes]
     features= numpy.asarray(features, dtype=numpy.float32 )
     classes= numpy.asarray( classes, dtype=numpy.float32 )
--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from extractor import get_feature_list, get_class_list
 from preprocessor import preprocess, preprocess_with_display
 from files import load_data_from_file, generate_ground_data
 
-image = cv2.imread('alpha.png')
-image2 = cv2.imread('alpha2.png')
+image = cv2.imread('data/alpha.png')
+image2 = cv2.imread('data/alpha2.png')
 copy=image.copy()
 image, segments = preprocess_with_display(image)
 image2, segments2 = preprocess(image2)
 knn = cv2.ml.KNearest_create()
 feature_list2 = get_feature_list(image, segments2)
-#generate_ground_data(image,copy,segments)
+generate_ground_data(image,copy,segments)
 classes, features = load_data_from_file()
 features2 =  numpy.asarray( feature_list2, dtype=numpy.float32 )
 knn.train(features,cv2.ml.ROW_SAMPLE, classes)
