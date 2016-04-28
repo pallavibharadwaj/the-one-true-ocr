732a3634d5a78435c9907d5121a0698cece0eaa2
bharadwajpallavi <pallavi.bharadwaj1594@gmail.com>
2016-04-28 13:54:39 +0530

    changed a few filenames and deleted unnecessary prints

--- files.py
+++ files.py
@@ -1,23 +1,19 @@
 import numpy
 from extractor import get_feature_list, get_class_list
 
-def generate_ground_data(in_file,image,copy, segments):
-    #print in_file
+def generate_ground_data(img_txt,image,copy, segments):
     feature_list= get_feature_list(image, segments)
     classes_list= get_class_list(copy, segments)
-    with open("%s" % in_file,'wb') as test :
+    with open("%s" % img_txt,'wb') as test :
         for char,feature in zip(classes_list,feature_list) :
-            print char  
             test.write("%s %s\n" %(chr(char), ' '.join(map(str,feature))))
 
-def load_data_from_file(in_file):
-    #print in_file
-    with open("%s" % in_file) as char_file:
+def load_data_from_file(img_txt):
+    #print img_txt
+    with open("%s" % img_txt) as char_file:
     	ncols = len(char_file.readline().split(' '))
-    classes = numpy.loadtxt("%s" %in_file, dtype = str, usecols = [0])
-    #print classes
-    features = numpy.loadtxt("%s" %in_file, dtype = float, usecols = range(1,ncols))
-    #print features
+    classes = numpy.loadtxt("%s" %img_txt, dtype = str, usecols = [0])
+    features = numpy.loadtxt("%s" %img_txt, dtype = float, usecols = range(1,ncols))
     classes = [ord(x) for x in classes]
     features= numpy.asarray(features, dtype=numpy.float32 )
     classes= numpy.asarray( classes, dtype=numpy.float32 )
