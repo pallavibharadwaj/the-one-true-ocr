09a8e3f453379c94a7a45fc7c3eb23d94e277b5c
heartz <ranadip.rulz@gmail.com>
2016-05-11 13:22:26 +0530

    Create function for formatting, remove few minor bugs

--- main.py
+++ main.py
@@ -5,6 +5,36 @@ from ocr import train, test, knnModel, SvmModel
 from extractor import get_feature_list
 from files import generate_ground_data
 
+def format_spaces(result,spaces_list,space_value):
+	newline_flag=1
+	final_string = ""
+	if(len(spaces_list)<=len(result)):
+		iterate_value = spaces_list
+		# Handles out of bounds in case of spaces < results
+	else:
+		iterate_value = result
+	for i in range(len(iterate_value)):
+		if newline_flag==1:
+			result_str= str(result[i])
+			newline_flag=0
+			continue
+		if spaces_list[i]>0 and spaces_list[i]<=space_value :
+			result_str+=str(result[i])
+			newline_flag=0
+		elif spaces_list[i]>space_value:
+			result_str+="  "+str(result[i])
+			newline_flag=0
+		elif spaces_list[i]<0:
+			result_str+=str(result[i]) # Add last char of each line
+			newline_flag=1
+			final_string+='\n'+ result_str
+
+	if(len(iterate_value)<len(result)):
+		final_string+='\n'+result_str
+		# Add last lines to images with lower spaces
+	final_string+=result[-1] # Last Character added to result
+	return final_string
+
 in_image = "data/alpha.png"
 test_feature_list,spaces_list= test(in_image)
 
@@ -13,45 +43,10 @@ class_list, feature_list = train(train_images)
 result = knnModel(feature_list,class_list,test_feature_list)
 result2 = SvmModel(feature_list,class_list,test_feature_list)
 print result
-
-print "Formatted result"
-
-newline_flag=1
-for i in range(len(result)):
-	if newline_flag==1:
-		result_str=str(result[i])
-		newline_flag=0
-		continue
-	if spaces_list[i]>0 and spaces_list[i]<=10 :
-		result_str=result_str+str(result[i])
-		newline_flag=0
-	elif spaces_list[i]>10:
-		result_str=result_str+"  "+str(result[i])
-		newline_flag=0
-	elif spaces_list[i]<0:
-		newline_flag=1
-		print result_str
-
 print result2
-
-print "Formatted result"
-
-newline_flag=1
-for i in range(len(result2)):
-	if newline_flag==1:
-		result_str2=str(result2[i])
-		newline_flag=0
-		continue
-	if spaces_list[i]>0 and spaces_list[i]<=10 :
-		result_str2=result_str2+str(result2[i])
-		newline_flag=0
-	elif spaces_list[i]>10:
-		result_str2=result_str2+"  "+str(result2[i])
-		newline_flag=0
-	elif spaces_list[i]<0:
-		newline_flag=1
-		print result_str2
-
+print "Formatted results"
+print(format_spaces(result,spaces_list,10))
+print(format_spaces(result2,spaces_list,10))
 #generate_ground_data("data/Agenda.png")
 cv2.waitKey(0)
 cv2.destroyAllWindows()
