60b2eefb4b5256fd42d6e0daf43ff960f387812f
heartz <ranadip.rulz@gmail.com>
2016-05-13 12:52:44 +0530

    Move Format space function to ocr.py

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
 import cv2
 import numpy
 from preprocessor import preprocess
-from ocr import train, test, knnModel, SvmModel
+from ocr import train, test, knnModel, SvmModel, format_spaces
 from extractor import get_feature_list
 from files import generate_ground_data
 
-def format_spaces(result,spaces_list,space_value):
-	newline_flag=1
-	final_string = ""
-	if(len(spaces_list)<=len(result)):
-		iterate_value = spaces_list
-		# Handles out of bounds in case of spaces < results
-	else:
-		iterate_value = result
-	for i in range(len(iterate_value)):
-		if newline_flag==1:
-			result_str= str(result[i])
-			newline_flag=0
-			continue
-		if spaces_list[i]>0 and spaces_list[i]<=space_value :
-			result_str+=str(result[i])
-			newline_flag=0
-		elif spaces_list[i]>space_value:
-			result_str+="  "+str(result[i])
-			newline_flag=0
-		elif spaces_list[i]<0:
-			result_str+=str(result[i]) # Add last char of each line
-			newline_flag=1
-			final_string+='\n'+ result_str
-
-	if(len(iterate_value)<len(result)):
-		final_string+='\n'+result_str
-		# Add last lines to images with lower spaces
-	final_string+=result[-1] # Last Character added to result
-	return final_string
 
 in_image = "data/alpha.png"
 test_feature_list,spaces_list= test(in_image)
@@ -46,7 +17,7 @@ print result
 print result2
 print "Formatted results"
 print(format_spaces(result,spaces_list,10))
-print(format_spaces(result2,spaces_list,10))
+print(format_spaces(result2,spaces_list))
 #generate_ground_data("data/Agenda.png")
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- ocr.py
+++ ocr.py
@@ -59,3 +59,34 @@ def SvmModel(training_features, training_classes, test_features):
 	result_classes = svm.predict(test_features)
 	results = [chr(int(val)) for val in result_classes]
 	return results
+
+
+def format_spaces(result,spaces_list,space_value=5):
+	newline_flag=1
+	final_string = ""
+	if(len(spaces_list)<=len(result)):
+		iterate_value = spaces_list
+		# Handles out of bounds in case of spaces < results
+	else:
+		iterate_value = result
+	for i in range(len(iterate_value)):
+		if newline_flag==1:
+			result_str= str(result[i])
+			newline_flag=0
+			continue
+		if spaces_list[i]>0 and spaces_list[i]<=space_value :
+			result_str+=str(result[i])
+			newline_flag=0
+		elif spaces_list[i]>space_value:
+			result_str+="  "+str(result[i])
+			newline_flag=0
+		elif spaces_list[i]<0:
+			result_str+=str(result[i]) # Add last char of each line
+			newline_flag=1
+			final_string+='\n'+ result_str
+
+	if(len(iterate_value)<len(result)):
+		final_string+='\n'+result_str
+		# Add last lines to images with lower spaces
+	final_string+=result[-1] # Last Character added to result
+	return final_string
